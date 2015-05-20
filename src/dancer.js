7eb57c9332d48ac1c5896363be248cf8f8c68664
stridentbean <borglin.me@gmail.com>
2015-05-20 16:06:30 -0700

    Fixed bugs in pseudoclassical instantiation

--- src/dancer.js
+++ src/dancer.js
@@ -3,9 +3,8 @@ var makeDancer = function(top, left, timeBetweenSteps){
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-
+  this.timeBetweenSteps = timeBetweenSteps;
   this.step();
-
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
@@ -14,7 +13,7 @@ var makeDancer = function(top, left, timeBetweenSteps){
 makeDancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(dancer.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 makeDancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
